27e684fba9dfa1b87b89a306467ef190ca652647
yuanrong1999 <55180626+yuanrong1999@users.noreply.github.com>
2020-03-31 14:28:10 +0800

    3/21 14:00 observe

---
+++ New Unity Project/Unity_ML_Soccer/Assets/Scripts/Ball.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public static bool complete; //足球是否進門
+
+    private void OnTriggerEnter(Collider other) //觸發開始事件:碰到勾選Is Trigger物件會執行一次
+    {
+        if(other.name== "Football_goal_cube") //若碰到物件為Football_goal_cube(球門感應區)is true,則進入球門
+        {
+            complete = true;
+        }
+    }
+}
---
+++ New Unity Project/Unity_ML_Soccer/Assets/Scripts/Robot.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using MLAgents;
+using MLAgents.Sensors;
+
+public class Robot : Agent
+{
+    private Rigidbody rigRobot; //機器人剛體
+    private Rigidbody rigBall; //足球剛體
+
+    private void Start()
+    {
+        rigRobot = GetComponent<Rigidbody>();
+        rigBall = GameObject.Find("Soccer Ball").GetComponent<Rigidbody>();
+    }
+    public override void OnEpisodeBegin() //事件開始時:重新設定機器人與足球位置
+    {
+        /*base.OnEpisodeBegin();*/
+        rigRobot.velocity = Vector3.zero;
+        rigRobot.angularVelocity = Vector3.zero;
+        rigBall.velocity = Vector3.zero;
+        rigBall.angularVelocity = Vector3.zero;
+
+        Vector3 posRobot = new Vector3(Random.Range(-2f, 2f), 0.1f, Random.Range(-2f, 0)); //Robot隨機位置
+        transform.position = posRobot;
+
+        Vector3 posBall = new Vector3(Random.Range(-2f, 2f), 0.1f, Random.Range(1f, 2f)); //Soccer剛體隨機位置
+        rigBall.position = posBall;
+    }
+}
