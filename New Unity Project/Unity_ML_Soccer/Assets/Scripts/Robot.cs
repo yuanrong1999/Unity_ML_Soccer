c8307a2e64343a59ee768d5417cb3fcd28cfe8a3
yuanrong1999 <55180626+yuanrong1999@users.noreply.github.com>
2020-04-19 03:56:24 +0800

    4/19

--- New Unity Project/Unity_ML_Soccer/Assets/Scripts/Robot.cs
+++ New Unity Project/Unity_ML_Soccer/Assets/Scripts/Robot.cs
@@ -4,6 +4,8 @@ using MLAgents.Sensors;
 
 public class Robot : Agent
 {
+    [Header("速度"), Range(1, 50)]
+    public float speed = 10;
     private Rigidbody rigRobot; //機器人剛體
     private Rigidbody rigBall; //足球剛體
 
@@ -25,5 +27,43 @@ public class Robot : Agent
 
         Vector3 posBall = new Vector3(Random.Range(-2f, 2f), 0.1f, Random.Range(1f, 2f)); //Soccer剛體隨機位置
         rigBall.position = posBall;
+
+        Ball.complate = false;//設事件開始時通關條件尚未成功(足球尚未進入球門)
     }
+
+    public override void CollectObservations(VectorSensor sensor)//事件開始時:重新設定機器人與足球位置
+    {
+        sensor.AddObservation(transform.position);
+        sensor.AddObservation(rigBall.position);
+        sensor.AddObservation(rigRobot.velocity.x);
+        sensor.AddObservation(rigRobot.velocity.z);
+    }
+
+    public override void AgentAction(float[] vectorAction)
+    {
+        Vector3 control = Vector3.zero;
+        control.x = vectorAction[0];
+        control.z = vectorAction[0];
+        rigRobot.AddForce(control * speed);
+
+        if (Ball.complate)
+        {
+            SetReward(1);
+            EndEpisode();
+        }
+        if (transform.position.y < 0 || rigBall.position.y < 0) //失敗 扣1分並結束(機器人or除足球掉至地板下)
+        {
+            SetReward(-1);
+            EndEpisode();
+        }
+    }
+
+    public override float[] Heuristic() //探索:讓開發者測試環境(官方提供的測試方式) 
+    {
+        var action = new float[2];
+        action[0] = Input.GetAxis("Horizontal");
+        action[1] = Input.GetAxis("Vertiacl");
+        return action;
+    }
+
 }
